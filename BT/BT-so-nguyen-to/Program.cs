acda092447982a8ce039f8f2e6d5aaae6a893bc7
unknown <phanphucan4890@gmail.com>
2020-07-03 14:44:26 +0700

    songuyento

---
+++ BT/BT-so-nguyen-to/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BT_so_nguyen_to
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 100; i++)
+            {
+                if (kiemtrasnt(i))
+                {
+                    Console.Write(i + ";");
+                }
+
+            }
+
+        }
+        public static bool kiemtrasnt(int number)
+        {
+            bool check = true;
+            if (number < 2)
+            {
+                check = false;
+            }
+
+            else
+            {
+                int i = 2;
+
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+
+            }
+            return check;
+        }
+    }
+}
