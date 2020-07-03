9fb085460ee2beeaed2a41bafe04ee271a5d7905
unknown <phanphucan4890@gmail.com>
2020-07-03 14:02:25 +0700

    docso

--- BT/BT-doc-so/Program.cs
+++ BT/BT-doc-so/Program.cs
@@ -3,72 +3,55 @@ namespace HelloWorld
 {
     public class docSoThanhChu
     {
-        public static void main()
+        public static void Main()
         {
-            Console.WriteLine("Enter number: ");
-            int number = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"{number} english reading is: {docChuSo(number)}");
+            Console.WriteLine("nhap so:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(docso(number));
         }
-        public static string doc1so(Int32 num)
+        public static string doc1so(int num)
         {
-            string bien = "a";
+            string doc = "doc1so";
             switch (num)
             {
                 case 0:
-                    bien = "";
+                    doc = "";
                     break;
                 case 1:
-                    bien = "one";
+                    doc = "one";
                     break;
                 case 2:
-                    bien = "two";
+                    doc = "two";
                     break;
                 case 3:
-                    bien = "three";
+                    doc = "three";
                     break;
                 case 4:
-                    bien = "four";
+                    doc = "four";
                     break;
                 case 5:
-                    bien = "five";
+                    doc = "five";
                     break;
                 case 6:
-                    bien = "six";
+                    doc = "six";
                     break;
                 case 7:
-                    bien = "seven";
+                    doc = "seven";
                     break;
                 case 8:
-                    bien = "eight";
+                    doc = "eight";
                     break;
                 case 9:
-                    bien = "nine";
-                    break;
-                case 10:
-                    bien = "ten";
-                    break;
-                case 11:
-                    bien = "eleven";
-                    break;
-                case 12:
-                    bien = "twelve";
-                    break;
-                case 15:
-                    bien = "fifteen";
-                    break;
-                case 13:
-                    bien = " thirteen";
-                    break;
-                case 20:
-                    bien = "twenty";
-                    break;
-                default:
-                    bien = doc1so(num % 10) + "teen";
+                    doc = "night";
                     break;
             }
-            return bien;
+
+            Console.WriteLine($"Doc 1 so: {doc}");
+
+            return doc;
         }
-        public static string doc2so(int num)
+
+        public static string doc2SoLonhon20(int num)
         {
             string doc = "doc2so";
             switch (num / 10)
@@ -79,61 +62,78 @@ namespace HelloWorld
                 case 3:
                     doc = "thirty " + doc1so(num % 10);
                     break;
+
                 case 4:
                     doc = "forty " + doc1so(num % 10);
                     break;
+
                 case 5:
                     doc = "fifty " + doc1so(num % 10);
                     break;
+
                 case 8:
                     doc = "eighty " + doc1so(num % 10);
                     break;
+
+
                 default:
-                    doc = $"{doc1so(num / 10)}ty {doc1so(num % 10)}";
+                    doc = doc1so(num / 10) + "ty " + doc1so(num % 10);
                     break;
+            }
+
+            Console.WriteLine($"Doc 2 so: {doc}");
+
+            return doc;
+        }
+        public static string doc2sonhohon20(int num)
+        {
+            string doc = "doc2sonhohon20";
+            switch (num)
+            {
+                case 10:
+                    doc = "ten";
+                    break;
+                case 11:
+                    doc = "eleven";
+                    break;
+                case 12:
+                    doc = "twelve";
+                    break;
+                case 13:
+                    doc = "thirteen";
+                    break;
+                case 15:
+                    doc = "fiften";
+                    break;
+
+                default:
+                    doc = doc1so(num / 10) + "teen";
+                    break;
+
             }
             return doc;
         }
+
         public static string doc3so(int num)
         {
             string doc = "doc3so";
-            string haiso = (num % 100) == 0 ? "" : (num % 100) <= 20 ? "and " + doc1so(num % 100) : "and " + doc2so(num % 100);
-            doc = $"{doc1so(num / 100)} hundreds {haiso}";
+            string duoi = (num % 100 > 20) ? "and " + doc2SoLonhon20(num % 100) : (num % 100 > 10) ? "and " + doc2sonhohon20(num % 100) : "and " + doc1so(num % 100);
+            doc = doc1so(num / 100) + " hundreeds" + duoi;
             return doc;
         }
-        public static string doc4so(int num)
-        {
-            string doc = "doc4so";
-            string baso = (num % 1000) == 0 ? "" : (num % 1000) <= 20 ? "and " + doc1so(num % 1000) : (num % 1000) < 100 ? "and " + doc2so(num % 1000) : doc3so(num % 1000);
-            doc = $"{doc1so(num / 1000)} thousands {baso} ";
-            return doc;
-        }
-        public static string doc5so(int num)
-        {
-            string doc = "doc5so";
-            string bonso = (num / 1000) <= 20 ? doc1so(num / 1000) : doc2so(num / 1000);
-            string duoi = (num % 1000) == 0 ? "" : (num % 1000) <= 20 ? "and " + doc1so(num % 1000) : (num % 1000) < 100 ? "and " + doc2so(num % 1000) : doc3so(num % 1000);
-            doc = $"{bonso} thousands {duoi} ";
-            return doc;
-        }
-        public static string docChuSo(int number)
+        public static string docso(int num)
         {
             string doc = "docso";
-            if (number == 0)
-            {
-                doc = "zero";
-            }
-            else if (number > 0)
-            {
-                if (number <= 20) { doc = doc1so(number); }
-                else if (number < 100) { doc = doc2so(number); }
-                else if (number < 1000) { doc = doc3so(number); }
-                else if (number < 10000) { doc = doc4so(number); }
-                else if (number < 100000) { doc = doc5so(number); }
-                else { doc = " Nhap so chua dung"; }
-            }
-            else { doc = "negative " + docChuSo(number * (-1)); }
+            if (num == 0) { doc = "zero"; }
+            else if (num < 10) { doc = doc1so(num); }
+            else if (num <= 20) { doc = doc2sonhohon20(num); }
+            else if (num < 100) { doc = doc2SoLonhon20(num); }
+            else if (num < 1000) { doc = doc3so(num); }
+            else { doc = "jjjjjjj"; }
             return doc;
         }
+
+
     }
+
 }
